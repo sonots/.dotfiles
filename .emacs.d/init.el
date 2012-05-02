02d7db6c8c9dae939d06657f03b34a49ab74c33b
nseo <sonots@gmail.com>
2012-05-02 05:50:15 -0700

    rst.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -236,3 +236,11 @@
 (require 'session)
 (add-hook 'after-init-hook 'session-initialize)
 
+;; rst.el
+(require 'rst)
+(setq auto-mode-alist
+      (append '(("\\.rst$" . rst-mode)
+      		("\\.rest$" . rst-mode)) auto-mode-alist))
+      		(setq frame-background-mode 'dark)
+      		(add-hook 'rst-mode-hook '(lambda() (setq indent-tabs-mode nil)))
+
