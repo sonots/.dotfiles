2ba5976fb2ba98396ba63cb771cbd99cd4dd6455
nseo <sonots@gmail.com>
2012-05-07 09:54:48 +0900

    refine ln.sh

--- ln.sh
+++ ln.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 [ -d .zsh/zaw ] || git clone git://github.com/zsh-users/zaw .zsh/zaw
-ln -s ~/.dotfiles/.* ~/
-
+for i in `ls -a`
+do
+  [ $i = "." ] && continue
+  [ $i = ".." ] && continue
+  [ $i = ".git" ] && continue
+  [ $i = "ln.sh" ] && continue
+  ln -s ~/.dotfiles/$i ~/
+done
