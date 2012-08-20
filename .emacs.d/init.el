cdddb4303a0dae40cef69af5607a8de48827d58a
nseo <sonots@gmail.com>
2012-08-20 14:14:23 +0900

    custom rst.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -279,3 +279,24 @@
 (define-key ctl-x-map "C" 'save-buffers-kill-emacs) ; C-x C to close without save windows
 ;(resume-windows 0)
 
+;; rst.el (custom color)
+(custom-set-faces
+;; custom-set-faces was added by Custom.
+;; If you edit it by hand, you could mess it up, so be careful.
+;; Your init file should contain only one such instance.
+;; If there is more than one, they won't work right.
+'(rst-level-1-face ((t (:foreground "LightSkyBlue"))) t)
+'(rst-level-2-face ((t (:foreground "LightGoldenrod"))) t)
+'(rst-level-3-face ((t (:foreground "Cyan1"))) t)
+'(rst-level-4-face ((t (:foreground "chocolate1"))) t)
+'(rst-level-5-face ((t (:foreground "PaleGreen"))) t)
+'(rst-level-6-face ((t (:foreground "Aquamarine"))) t))
+;; (rst-level-7-face ((t (:foreground "LightSteelBlue"))) t)  ;; メモ
+;; (rst-level-8-face ((t (:foreground "LightSalmon"))) t)
+(custom-set-variables
+;; custom-set-variables was added by Custom.
+;; If you edit it by hand, you could mess it up, so be careful.
+;; Your init file should contain only one such instance.
+;; If there is more than one, they won't work right.
+'(rst-level-face-base-light 50))
+
