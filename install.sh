b6666ae79708bd595c3bd697646ac93bcd4e0adc
sonots <sonots@gmail.com>
2020-09-11 02:54:44 +0900

    brew install krew

--- install.sh
+++ install.sh
@@ -52,14 +52,4 @@ if [ $(uname) = "Darwin" ]; then
   defaults write com.apple.finder AppleShowAllFiles TRUE
 fi
 
-# krew
-(
-  set -x; cd "$(mktemp -d)" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.{tar.gz,yaml}" &&
-  tar zxvf krew.tar.gz &&
-  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
-  "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
-  "$KREW" update
-)
-
 popd
